9b63a8f67145cf1db2492725532b9b2d32ac3902
Ynka123 <541231219@student.smktelkom-pwt.sch.id>
2024-10-16 14:27:22 +0700

    add index and login page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Home - Explore Top Movies and Series | Watch Now</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Discover your favorite movies and series by genre and year on our landing page. Featuring top-rated superhero, fantasy, and sci-fi titles with IMDB ratings. Watch the latest with stars like Tom Holland and Zendaya."
+    />
+    <img
+      alt="Logo"
+      src="https://appproject.dhiwise.com/dhiwise-logo.png?c=&v="
+      style="width: 0px; height: 0px; display: none"
+    />
+    <link rel="stylesheet" type="text/css" href="ASET/css/index.css" />
+    <link rel="stylesheet" type="text/css" href="ASET/css/font.css" />
+    <link rel="stylesheet" type="text/css" href="ASET/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="ASET/css/components.css" />
+    <link rel="stylesheet" type="text/css" href="ASET/css/Landingpage.css" />
+  </head>
+  <body>
+    <!-- main content section -->
+    <div class="landing-page">
+      <!-- header section -->
+      <div class="stackheaderlogo">
+        <!-- navigation section -->
+        <div class="rowsearch">
+          <div class="column">
+            <header class="header">
+              <div class="header__top-row">
+                <img src="ASET/images/img_header_logo.svg" alt="Header Logo" class="header__logo" />
+                <div class="header__navigation">
+                  <ul class="header__menu">
+                    <li>
+                      <a href="#">
+                        <p class="ui text size-texts">HOME</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <p class="ui text size-texts">GENRE</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <p class="ui text size-texts">YEAR</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <p class="ui text size-texts">MOVIE</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <p class="ui text size-texts">SERIES</p>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <label class="header__search">
+                <img src="ASET/images/img_rewind.svg" alt="Rewind" class="rewind" />
+                <input name="Search Bar" placeholder="search here" type="text" />
+              </label>
+            </header>
+            <img src="ASET/images/img_logo_spiderman.png" alt="Logo Image" class="column__logo" />
+          </div>
+        </div>
+
+        <!-- footer section -->
+        <footer class="footer">
+          <div class="footer__column">
+            <div class="footer__row">
+              <button class="flex-row-center-center footer__button">
+                <img src="ASET/images/img_playcircle.svg" alt="Play Circle" class="play_circle" />
+                <span> watch</span>
+              </button>
+              <button class="footer__icon-button--grid">
+                <img src="ASET/images/img_grid.svg" />
+              </button>
+              <button class="footer__icon-button--television">
+                <img src="ASET/images/img_television.svg" />
+              </button>
+            </div>
+            <p class="footer__text--superhero ui text size-texts">
+              superhero - supernatural fantasy - urban adventure - action - adventure - fantasy - SCI-FI
+            </p>
+            <p class="footer__text--rating ui text size-textxs">IMDB : 8,2/10</p>
+            <p class="ui text size-textxs">Cast : Tom Holland, Zendaya, Benedict Cumberbatch</p>
+            <p class="footer__text--description ui text size-textmd">
+              when his loved ones are in a dangerous situation, Peter<br />Parker asks Doctor Strange to give him back
+              his secret and <br />accidentally causing a big mess
+            </p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); // ke index blade php
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); // ke welcome kalo diketik welcome
+});
+
+Route::get('/login', function () {
+    return view('login'); // ke login kalo diketik login
 });
